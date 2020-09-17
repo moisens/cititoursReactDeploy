9e5e1d98f5290acf0b2e6879928d07d75b98f689
moise <mose.ns@hotmail.com>
2020-09-17 15:00:42 +0200

    cityTours

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.scss';
+import Navbar from './components/Navbar/Navbar'
+import TourList from './components/TourList'
+
+
+
+
+
+
+
+function App() {
+  return (
+    <React.Fragment>
+      <Navbar />
+      <TourList />
+    </React.Fragment>
+    
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import logo from '../../logo.svg'
+import './navbar.scss'
+
+
+function Navbar() {
+  return (
+    <nav className='navbar'>
+        <img src={logo} alt='city tours company' />
+        <ul className='nav-links'>
+            <li><a href='/' className='nav-link'>home</a></li>
+            <li><a href='/' className='nav-link'>about</a></li>
+            <li><a href='/' className='nav-link active'>tours</a></li>
+        </ul>
+
+    </nav>
+  )
+}
+
+
+export default Navbar;
